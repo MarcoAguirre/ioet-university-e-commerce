6fb959db2c0870a612a58df07da3c01b70fc0ad6
MarcoAguirre <viniciosoria16@gmail.com>
2024-03-12 01:18:20 -0500

    feat: include add to cart button to each product card

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,41 +1,52 @@
-import { useContext } from 'react';
-import './Modal.css'
-import { Rating } from "../Filter/RatingFilter/Rating"
-import { AiOutlineCloseCircle } from 'react-icons/ai'
-import { SearchContext } from '../../contexts/SearchContext';
+import { useContext } from "react";
+import "./Modal.css";
+import { Rating } from "../Filter/RatingFilter/Rating";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { SearchContext } from "../../contexts/SearchContext";
+import { Button } from "../Button/Button";
 
-function Modal () {
-    const {
-        setIsOpen,
-        imageProduct,
-        titleProduct,
-        priceProduct,
-        descriptionProduct,
-        rateProduct
-    } = useContext(SearchContext);
+function Modal() {
+  const {
+    setIsOpen,
+    imageProduct,
+    titleProduct,
+    priceProduct,
+    descriptionProduct,
+    rateProduct,
+  } = useContext(SearchContext);
 
-    const setCloseModal = () => {
-        setIsOpen(false)
-    }
+  const setCloseModal = () => {
+    setIsOpen(false);
+  };
 
-    return (
-        <div className='ModalContainer'>
-            <div className='ContentModalContainer'>
-                <div className='ProductImageContainer'>
-                    <AiOutlineCloseCircle className='closeModal' onClick={setCloseModal}/>
-                    <img src={imageProduct}/>
-                </div>
-                <div className="DetailsModalContainer">
-                    <div className="HeaderDetailModalContainer">
-                        <h3>{titleProduct}</h3>
-                        <h3>${priceProduct}</h3>
-                    </div>
-                    <Rating stars={rateProduct}/>
-                    <h6>{descriptionProduct}</h6>
-                </div>
-            </div>
+  const addToCart = () => {
+    // Here will be the functionality for the add to cart button
+  };
+
+  return (
+    <div className="ModalContainer">
+      <div className="ContentModalContainer">
+        <div className="ProductImageContainer">
+          <AiOutlineCloseCircle
+            className="closeModal"
+            onClick={setCloseModal}
+          />
+          <img src={imageProduct} />
         </div>
-    )
+        <div className="DetailsModalContainer">
+          <div className="HeaderDetailModalContainer">
+            <h3>{titleProduct}</h3>
+            <h3>${priceProduct}</h3>
+          </div>
+          <Rating stars={rateProduct} />
+          <h6>{descriptionProduct}</h6>
+          <div style={{ display: "flex", justifyContent: "right" }}>
+            <Button text="Add" color="success" onClick={addToCart}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export { Modal }
+export { Modal };
